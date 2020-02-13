8455070634b81923870879ad849492e74ef970d3
isak_01 <t-yuta08780126@outlook.jp>
2020-02-13 16:25:14 +0900

    カウンター追加

---
+++ works/counter.js
@@ -0,0 +1,9 @@
+let count = 0
+const countUp = () => {
+  count++
+  const countElement = document.querySelector('#count')
+  countElement.innerText = '回数: ' + count
+}
+const reset = () => {
+    count = 0
+}
